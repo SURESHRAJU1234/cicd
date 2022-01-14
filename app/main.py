edce99b77aa7e4d0e9962d2f35ba8488cc0811ef
SURESHRAJU1234 <47245207+SURESHRAJU1234@users.noreply.github.com>
2022-01-14 12:53:27 -0500

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,9 +1,14 @@
+import platform
+from datetime import datetime
 from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello from Python!"
+    now = datetime.now() # current date and time
+    date_time = now.strftime("%m/%d/%Y, %H:%M:%S")
+    message = "You did it. Welcome to Hello world of "+platform.node() + ". Current time is " +date_time
+    return message
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
