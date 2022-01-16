aec263a51c90c584fea7eff94154801f2267abaf
SURESHRAJU1234 <47245207+SURESHRAJU1234@users.noreply.github.com>
2022-01-16 17:45:44 -0500

    Update main.py

--- app/main.py
+++ app/main.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 def hello():
     now = datetime.now() # current date and time
     date_time = now.strftime("%m/%d/%Y, %H:%M:%S")
-    message = "You did it. Welcome to Hello world of Kubernetes demo "+platform.node() + ". Current time is " +date_time
+    message = "Today is a good day. You did it. Welcome to Hello world of Kubernetes demo "+platform.node() + ". Current time is " +date_time
     return message
 
 if __name__ == "__main__":
